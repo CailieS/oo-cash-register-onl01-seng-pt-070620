943f7305f9b210ee409a055670478f373a7fd22f
Cailie Skelton <cskelton707@gmail.com>
2020-08-11 02:54:56 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,6 +9,18 @@ class CashRegister
     @last_item = [] 
   end
   
+   def add_item(title, price, quantity=1)
+    @total += price * quantity 
+    quantity.times do 
+      @items << title 
+    end 
+    @last_item = price * quantity 
+  end 
+  
+  def items 
+    @items 
+  end 
+  
   def apply_discount
     if discount != 0 
       @total -= (@total * (@discount / 100.to_f))
