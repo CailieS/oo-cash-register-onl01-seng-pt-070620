89ab0edfb6b46bfb127ec2624720a50b9a374e06
Cailie Skelton <cskelton707@gmail.com>
2020-08-11 02:54:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,14 @@ class CashRegister
     @last_item = [] 
   end
   
-  
+  def apply_discount
+    if discount != 0 
+      @total -= (@total * (@discount / 100.to_f))
+      "After the discount, the total comes to $#{@total.to_i}."
+    else 
+      "There is no discount to apply."
+    end 
+  end 
   
   
   
